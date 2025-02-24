a20e4d1396af531bd915b167975d61f5b260113e
Monisha <thenameismonisha@gmail.com>
2025-02-24 16:18:23 +0530

    added all files

---
+++ dashboard/src/components/Orders.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+import { Link } from "react-router-dom";
+
+const Orders = () => {
+
+  const [allOrders,setAllOrders] = useState([]);
+  useEffect(()=>{
+    axios.get("http://localhost:3002/allOrders").then((res)=>{
+      setAllOrders(res.data);
+    })
+  },[]);
+
+
+  return (
+    // <div className="orders">
+    //   <div className="no-orders">
+    //     <p>You haven't placed any orders today</p>
+
+    //     <Link to={"/"} className="btn">
+    //       Get started
+    //     </Link>
+    //   </div>
+    // </div>
+    <>
+    <h3 className="title">Orders ({allOrders.length})</h3>
+
+      <div className="order-table">
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Mode</th>
+          </tr>
+          {allOrders.map((stock, index) => {
+
+            return (
+              <tr key={index}>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td >{stock.mode}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+      </>
+  );
+};
+
+export default Orders;
---
+++ readme.md
@@ -0,0 +1,9 @@
+frontend - html,css,js,react,bootstrap,material UI
+backend - node, expressJS, mongoDB, 
+Testing - Jest
+Deployment - aws
+
+put a check to buy a stock and handle properly when stock is not bought if money is less
+enable sell option, to sell , quantity should be available 
+bought stock should go to holdings
+login and signup
